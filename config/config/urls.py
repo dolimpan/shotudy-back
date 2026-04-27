3fd8a477b936d1c73a0b9be1463a8aec7e6832de
dolimpan <146081854+dolimpan@users.noreply.github.com>
2026-04-27 04:09:41 +0000

    req.

--- config/config/urls.py
+++ config/config/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include('api.urls'))
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema')),
 ]
