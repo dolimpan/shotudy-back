5704f8d5e99520aed9805be14c8920c9110606d5
dolimpan <146081854+dolimpan@users.noreply.github.com>
2026-06-04 11:44:13 +0000

    1

--- config/config/settings.py
+++ config/config/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
+ALLOWED_HOSTS = [ 
+    "api.shotudy.site",
     "shotudy.site",
     "www.shotudy.site",
     "127.0.0.1",
